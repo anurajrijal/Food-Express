7d4244417b7e41374180cc251619c70854199ece
anurajrijal <anurajrijla@gmail.com>
2024-08-05 10:43:51 +0545

    added pages and cards

--- FoodExpress/src/Pages/Home/Home.jsx
+++ FoodExpress/src/Pages/Home/Home.jsx
@@ -3,17 +3,18 @@ import React from 'react'
 function Home() {
     return (
         <>      
-        <div className="mx-auto w-full max-w-7xl">
-            <aside className="relative overflow-hidden text-black rounded-lg sm:mx-16 mx-2 sm:py-16">
+        
+        <div className="mx-auto w-full max-w-2xl">
+           
                 <div className="relative z-10 max-w-screen-xl px-4  pb-20 pt-10 sm:py-24 mx-auto sm:px-6 lg:px-8">
                     <div className="max-w-xl sm:mt-1 mt-80 space-y-8 text-center sm:text-right sm:ml-auto">
-                        <h2 className="text-4xl font-bold sm:text-5xl">
-                            Download Now
-                            <span className="hidden sm:block text-4xl">Lorem Ipsum</span>
-                        </h2>
-                    </div>
-                </div>
-            </aside>
+        <img
+                            src="https://cdn.leonardo.ai/users/5e3ec570-11e2-4cad-a7cc-5ef68f18753e/generations/b1e43659-b07b-4b99-b101-9749c575dbb8/Default_a_logo_of_Food_Express_0.jpg?w=512"
+                          
+                        />
+        </div>
+        </div>
+        
         </div>
         
         </>
