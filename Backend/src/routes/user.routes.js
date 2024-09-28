b0ce0de3fbf62f61217113e9f510e6419838f0b7
nirajacharyaa <mail.nirajacharya@gmail.com>
2024-09-29 00:13:38 +0545

    fix: undefined googleId

--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,31 +1,31 @@
 import { Router } from "express";
 import passport from "passport";
-import { 
+import {
     loginUser,
     logoutUser,
     registerUser,
-    refreshAccessToken, 
-    changeCurrentPassword, 
-    getCurrentUser, 
-    updateAccountDetails, 
-    updateUserAvatar, 
-   } from "../controllers/user.controller.js";
-import {upload} from "../middlewares/multer.middleware.js";
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+} from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { manageUserRole, getAllUsers,deleteUser } from "../controllers/admin.controller.js";
+import { manageUserRole, getAllUsers, deleteUser } from "../controllers/admin.controller.js";
 import { authorizeRoles } from "../middlewares/role.middleware.js";
 import { User } from "../models/user.model.js";
-const router=Router()
+const router = Router()
 
 router.route("/register").post(
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
         },
         {
-            name:"coverImage",
-            maxCount:1
+            name: "coverImage",
+            maxCount: 1
         }
     ]),
     registerUser)
@@ -39,7 +39,7 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT, changeCurrentPassword)
 router.route("/current-user").get(verifyJWT, getCurrentUser)
 router.route("/update-account").patch(verifyJWT, updateAccountDetails)
-router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateUserAvatar)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
 
 
 
@@ -49,7 +49,7 @@ router.get('/auth/google', passport.authenticate('google', { scope: ['profile', 
 router.get('/auth/google/callback', passport.authenticate('google', { session: false }), async (req, res) => {
     // Google OAuth callback logic
     try {
-        let user = await User.findOne({ googleId: req.user.googleId });
+        let user = await User.findOne({ googleId: req.user.user.googleId });
 
         if (!user) {
             user = new User({
