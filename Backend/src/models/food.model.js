7b6a8ae35150650588b47bfa09a924226c73b2f4
anurajrijal <anurajrijla@gmail.com>
2024-10-01 14:03:36 +0545

    foodmodel

--- Backend/src/models/food.model.js
+++ Backend/src/models/food.model.js
@@ -1,6 +1,26 @@
 import mongoose, { Schema } from "mongoose";
 
 const foodSchema = new Schema({
-    
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    fimage:{
+        type:String,
+        required:true
+    },
+    cookTime:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
 
 },{timestamps:true})
+export const Food = mongoose.model("Food",foodSchema)
